4dddbcc2dfaad24d8cc363da1b59f47d5642ce02
Natasha Rocha <nattycdb@hotmail.com>
2017-12-23 16:46:51 -0200

    refactor ContactHeaderCard
    Card now shows phone number if contact doesnt have an email address

--- src/components/ContactHeaderCard.js
+++ src/components/ContactHeaderCard.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 
 class ContactHeaderCard extends Component {
   render() {
-    let contact = this.props.contact
+    let { contact } = this.props
 
     // Colocando iniciais em maiusculo
     let firstName = contact.name.first.charAt(0).toUpperCase() + contact.name.first.slice(1)
@@ -13,7 +13,7 @@ class ContactHeaderCard extends Component {
         <img src={contact.picture.medium} alt="" />
         <div>
           <p>{`${firstName} ${lastName}`}</p>
-          <p>{contact.email}</p>
+          {contact.email ? <p>{contact.email}</p> : <p>{contact.phone}</p>}
         </div>
       </div>
     )
