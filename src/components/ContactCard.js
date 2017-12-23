c62783d093595d2c79a3c253241aef32bd53f263
Natasha Rocha <nattycdb@hotmail.com>
2017-12-23 16:41:05 -0200

    implement ContactCard

--- src/components/ContactCard.js
+++ src/components/ContactCard.js
@@ -2,10 +2,27 @@ import React, { Component } from 'react';
 import ContactHeaderCard from './ContactHeaderCard.js';
 
 class ContactCard extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {active: false}
+  }
+
+  handleClick = (e) => {
+    e.preventDefault()
+    this.setState((prevState) => ({active: !prevState.active}))
+  }
+
   render() {
+    let { contact } = this.props
+
     return (
-      <div>
-        <ContactHeaderCard contact={this.props.contact} />
+      <div onClick={this.handleClick}>
+        <ContactHeaderCard contact={contact} />
+        {this.state.active && (
+          <div>
+            {contact.phone && <div><b>Phone Number:</b> {contact.phone}</div>}
+            {contact.cell && <div><b>Cellphone:</b> {contact.cell}</div>}
+          </div>)}
       </div>
     )
   }
