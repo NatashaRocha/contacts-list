f5b62e14a3a574819dade6b6936e93eaa8f677e1
Natasha Rocha <nattycdb@hotmail.com>
2017-12-23 09:51:09 -0200

    build architecture

---
+++ src/components/ContactCard.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class ContactCard extends Component {
+  render() {
+    return <div></div>
+  }
+}
+
+export default ContactCard;
---
+++ src/components/ContactsList.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class ContactsList extends Component {
+  render() {
+    return <div></div>
+  }
+}
+
+export default ContactsList;
