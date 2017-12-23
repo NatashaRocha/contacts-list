784c2007a8141a044579badd9dc7f1463d96b48a
Natasha Rocha <nattycdb@hotmail.com>
2017-12-23 17:19:23 -0200

    lift active state from ContactCard
    I did this so that no more than one card can be active at the same time.

--- src/components/ContactCard.js
+++ src/components/ContactCard.js
@@ -2,23 +2,13 @@ import React, { Component } from 'react';
 import ContactHeaderCard from './ContactHeaderCard.js';
 
 class ContactCard extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {active: false}
-  }
-
-  handleClick = (e) => {
-    e.preventDefault()
-    this.setState((prevState) => ({active: !prevState.active}))
-  }
-
   render() {
-    let { contact } = this.props
+    let { contact, active, onClick } = this.props
 
     return (
-      <div onClick={this.handleClick}>
+      <div onClick={onClick}>
         <ContactHeaderCard contact={contact} />
-        {this.state.active && (
+        {active && (
           <div>
             {contact.phone && <div><b>Phone Number:</b> {contact.phone}</div>}
             {contact.cell && <div><b>Cellphone:</b> {contact.cell}</div>}
--- src/components/ContactsList.js
+++ src/components/ContactsList.js
@@ -2,11 +2,25 @@ import React, { Component } from 'react';
 import ContactCard from './ContactCard.js'
 
 class ContactsList extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {active: ""}
+  }
+
+  handleClick = (e, username) => {
+    e.preventDefault()
+    username === this.state.active ? this.setState({active: ""}) : this.setState({active: username})
+  }
+
   render() {
     return (
       <ul>
         {this.props.contacts.map((contact) => (
-          <ContactCard contact={contact} key={contact.login.username}/>
+          <ContactCard
+            contact={contact}
+            key={contact.login.username}
+            active={this.state.active === contact.login.username}
+            onClick={(e) => {this.handleClick(e, contact.login.username)}}/>
           ))
         }
       </ul>
