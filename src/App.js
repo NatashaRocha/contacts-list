ad80cda4ab5dd837f83299f6d04255f9129da556
Natasha Rocha <nattycdb@hotmail.com>
2017-12-23 10:30:13 -0200

    mock data

--- src/App.js
+++ src/App.js
@@ -3,11 +3,27 @@ import ContactsList from './components/ContactsList.js';
 import SearchBar from './components/SearchBar.js';
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      contacts: [],
+    }
+  }
+
+  componentWillMount() {
+    fetch("http://www.mocky.io/v2/5a3e45532f00003a0e17136f")
+    .then((results) => (results.json()))
+    .then((data) => {
+      let contacts = data.results
+      this.setState({contacts: contacts})
+    })
+  }
+
   render() {
     return (
       <div>
-        <SearchBar />
-        <ContactsList />
+        <SearchBar contacts={this.state.contacts} />
+        <ContactsList contacts={this.state.contacts} />
       </div>
     );
   }
