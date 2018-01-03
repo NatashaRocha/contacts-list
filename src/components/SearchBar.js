6f6b9d2b718e82949afccf6bfe4492094783c6d8
Natasha Rocha <nattycdb@hotmail.com>
2018-01-02 22:45:55 -0200

    fix #5 prevent default on SearchBar

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
 
 class SearchBar extends Component {
+  onSubmit = (e) => {e.preventDefault()}
   render() {
     return (
-      <form className="search-bar">
+      <form className="search-bar" onSubmit={this.onSubmit}>
         <input type="text" placeholder="Pesquisar" onChange={this.props.onChange}/>
       </form>)
   }
